42fec7f7e85f4ee9a9b630267cdc34739d2f3977
Torbjørn Birch Moltu <t.b.moltu@lyse.net>
2020-07-22 14:44:37 +0200

    (WiP) Support cross-compiling and test i686 build
    Need a separate .S file and post-processing arguments.

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
-
 set -e
 
+echo $#
+cargo_out_dir="target/release"
+if [ $# -eq 1 ]; then
+   extra_cargo_args="--target $1"
+   cargo_out_dir="target/$1/release"
+elif [ $# -ge 2 ]; then
+   echo "Too many arguments" >&2
+   exit 1
+fi
+
 for d in rustc cargo ar ld objcopy nasm hexdump; do
-    which $d >/dev/null || (echo "Can't find $d, needed to build"; exit 1)
+    which $d >/dev/null || (echo "Can't find $d, needed to build" >&2; exit 1)
 done
 
 printf "Tested on rustc 1.46.0-nightly (346aec9b0 2020-07-11)\nYou have  "
@@ -12,11 +21,11 @@ echo
 
 set -x
 
-cargo build --release --verbose
+cargo build --release --verbose $extra_cargo_args
 
 # tinyrust.tinyrust.3a1fbbbh-cgu.0.rcgu.o
-OBJECT=$(ar t target/release/libtinyrust.rlib | grep '.o$')
-ar x target/release/libtinyrust.rlib "$OBJECT"
+OBJECT=$(ar t "$cargo_out_dir"/libtinyrust.rlib | grep '.o$')
+ar x "$cargo_out_dir"/libtinyrust.rlib "$OBJECT"
 
 objdump -dr "$OBJECT"
 echo
